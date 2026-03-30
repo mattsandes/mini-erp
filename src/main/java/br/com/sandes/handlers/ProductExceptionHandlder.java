157cc5924bcb563b47eaae5d70bb0db0752c1696
Mateus Sandes <mateussandes@hotmail.com.br>
2026-03-30 10:30:07 -0300

    Criado o exception handler da aplicaçao

---
+++ src/main/java/br/com/sandes/handlers/ProductExceptionHandlder.java
@@ -0,0 +1,42 @@
+package br.com.sandes.handlers;
+
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import br.com.sandes.exceptions.DuplicatedProductCodeException;
+import br.com.sandes.exceptions.ExceptionResponse;
+import br.com.sandes.exceptions.RequiredObjectNullException;
+
+@RestControllerAdvice
+public class ProductExceptionHandlder extends ResponseEntityExceptionHandler {
+	
+	@ExceptionHandler(Exception.class)
+	private final ResponseEntity<ExceptionResponse> handleInteralServerErrorException (
+			Exception ex, WebRequest request) {
+		
+		ExceptionResponse response = new ExceptionResponse(
+				new Date(), ex.getMessage(), request.getDescription(false));
+		
+		return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
+	@ExceptionHandler({
+		DuplicatedProductCodeException.class,
+		RequiredObjectNullException.class
+	})
+	private final ResponseEntity<ExceptionResponse> handleBadRequestException (
+			Exception ex, WebRequest request) {
+		
+		ExceptionResponse response = new ExceptionResponse(
+				new Date(), ex.getMessage(), request.getDescription(false));
+		
+		return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+	}
+	
+}
