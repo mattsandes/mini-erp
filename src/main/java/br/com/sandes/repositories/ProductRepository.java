cee60eb9b113c586a71c50265a0042ced49e8fce
Mateus Sandes <mateussandes@hotmail.com.br>
2026-03-30 10:25:44 -0300

    Criado o metodo existsByProductCode

--- src/main/java/br/com/sandes/repositories/ProductRepository.java
+++ src/main/java/br/com/sandes/repositories/ProductRepository.java
@@ -4,4 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.sandes.data.Product;
 
-public interface ProductRepository extends JpaRepository<Product, Long>{}
+public interface ProductRepository extends JpaRepository<Product, Long>{
+	
+	boolean existsByProductCode(String productCode);
+}
