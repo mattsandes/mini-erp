fd0f822be20f6c334995085142b7c32f4013d7a6
Mateus Sandes <mateussandes@hotmail.com.br>
2026-04-01 00:53:52 -0300

    Criada a exception para quando não for possivel encontrar clientes

---
+++ src/main/java/br/com/sandes/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,19 @@
+package br.com.sandes.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class ResourceNotFoundException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	public ResourceNotFoundException (String message) {
+		super(message);
+	}
+
+	public ResourceNotFoundException() {
+		super("No resources were found with the given ID");
+	}
+
+}
