41b38b4c3e9a25ae8bab376cac92aa783440d2e5
Mateus Sandes <mateussandes@hotmail.com.br>
2026-04-03 01:26:05 -0300

    Alterado o mapping para libera rtodos os controllers no cors

--- src/main/java/br/com/sandes/config/WebConfig.java
+++ src/main/java/br/com/sandes/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry
-			.addMapping("/api/product")
+			.addMapping("/**")
 			.allowedOrigins("http://localhost:4200")
 			.allowedMethods("*");
 	}
